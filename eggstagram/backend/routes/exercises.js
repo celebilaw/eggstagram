659bb51c44cdcede891219c123c4af2c0f4f46ee
Noah Gardner <noahgardner303@gmail.com>
2022-02-10 12:57:06 -0800

    test again

--- eggstagram/backend/routes/exercises.js
+++ eggstagram/backend/routes/exercises.js
@@ -34,27 +34,28 @@ router.route('/:id').get((req, res) => {
       .catch(err => res.status(400).json('Error: ' + err));
   });
   
-  // delete specific exercise
-  router.route('/:id').delete((req, res) => {
-    Exercise.findByIdAndDelete(req.params.id)
-      .then(() => res.json('Exercise deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+// delete specific exercise
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
   
-  // update specific exercise
-  router.route('/update/:id').post((req, res) => {
-    Exercise.findById(req.params.id)
-      .then(exercise => {
-        exercise.username = req.body.username;
-        exercise.description = req.body.description;
-        exercise.duration = Number(req.body.duration);
-        exercise.date = Date.parse(req.body.date);
-  
-        exercise.save()
-          .then(() => res.json('Exercise updated!'))
-          .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+// update specific exercise
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
+// hey
