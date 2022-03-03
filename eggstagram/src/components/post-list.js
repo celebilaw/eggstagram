62a21b3f9b9cdd2e45fd6cd232f6d7aba6d2f32a
celebi law <celebilaw@gmail.com>
2022-03-02 22:29:57 -0800

    fixed bug with the size of cards and their images

--- eggstagram/src/components/post-list.js
+++ eggstagram/src/components/post-list.js
@@ -59,8 +59,8 @@ export default class PostsList extends React.Component {
                     </tbody>
                 <div class="row g-4 mb-4">
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://spoonuniversity.com/wp-content/uploads/sites/61/2015/11/flat-fread-1024x1024.png" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://spoonuniversity.com/wp-content/uploads/sites/61/2015/11/flat-fread-1024x1024.png" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">De Neve Pizza</h5>
                                 <p class="card-text"> yoloadsfasdfsfsdfsdfasdfasdfsadfasdfasdfasdfsdafsdfsdafasdfasdfasdsdaasdfasdfsdafsdafasdfafasdfasdfasdfasdfsadfsdasdfasdfsadfsadfsadfasdfasdfsadfasdfasdfasdfasdfsadfdssdafasdfasfasdfasdfasfasdfsadfsadfsd </p>
@@ -68,8 +68,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://wp.dailybruin.com/images/2018/10/web.ns_.swipereward.RH_.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://wp.dailybruin.com/images/2018/10/web.ns_.swipereward.RH_.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">BPlate burgers</h5>
                                 <p class="card-text"> lalalaalala </p>
@@ -77,8 +77,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://pbs.twimg.com/media/Ee36c0VUEAU-_sZ.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://pbs.twimg.com/media/Ee36c0VUEAU-_sZ.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">This food sucks!</h5>
                                 <p class="card-text"> lalalaalala </p>
@@ -86,8 +86,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://pbs.twimg.com/media/Ee36c0VUEAU-_sZ.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://pbs.twimg.com/media/Ee36c0VUEAU-_sZ.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">This food sucks!</h5>
                                 <p class="card-text"> lalalaalala </p>
@@ -97,8 +97,9 @@ export default class PostsList extends React.Component {
                 </div>
                 <div class="row g-4 mb-4">
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20154/North-African-Style-Honey-R.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20154/North-African-Style-Honey-R.jpg"
+                            alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">De Neve Pizza</h5>
                                 <p class="card-text"> doododo deez </p>
@@ -106,8 +107,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20154/554a8675299b50588c0011e0_Polenta-Panzanella-Salad/Polenta-Panzanella-Salad_318c3ae6-0c00-498f-86af-bffc134443ab-prv.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://s3.amazonaws.com/cms.ipressroom.com/173/files/20154/554a8675299b50588c0011e0_Polenta-Panzanella-Salad/Polenta-Panzanella-Salad_318c3ae6-0c00-498f-86af-bffc134443ab-prv.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">BPlate burgers</h5>
                                 <p class="card-text"> lalalaalala </p>
@@ -115,8 +116,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="https://pbs.twimg.com/media/C4PfmHdVMAApMj4.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="https://pbs.twimg.com/media/C4PfmHdVMAApMj4.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">This food sucks!</h5>
                                 <p class="card-text"> lalalaalala </p>
@@ -124,8 +125,8 @@ export default class PostsList extends React.Component {
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-6 col-lg-3">
-                        <div class="card border-light bg-dark text-white">
-                            <img src="http://menu.dining.ucla.edu/Content/Images/RecipeImages/123084.jpg" alt="food image" class="card-img-top"/>
+                        <div class="card border-light bg-dark text-white card-size">
+                            <img src="http://menu.dining.ucla.edu/Content/Images/RecipeImages/123084.jpg" alt="food image" class="card-img-top card-img"/>
                             <div class="card-body overflow-auto">
                                 <h5 class="card-title">This food sucks!</h5>
                                 <p class="card-text"> lalalaalala </p>
